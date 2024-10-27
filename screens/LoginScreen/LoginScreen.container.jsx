34a703432c54360801cae735f52025d96fd2113d
Berkan Serbes <berkan_serbes@hotmail.com>
2024-10-27 11:12:55 +0300

    feat(auth): Add login functionality with validation and error handling
    - Implemented `handleLogin` function to manage user authentication
    - Added input validation for email and password fields
    - Displayed toast notifications for login success and error messages
    - Integrated with Redux for state management and navigation to "Main" screen on successful login
    - Added loading state and error handling to improve user experience

--- screens/LoginScreen/LoginScreen.container.jsx
+++ screens/LoginScreen/LoginScreen.container.jsx
@@ -1,18 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import LoginScreenUI from "./LoginScreen.ui";
 import { useNavigation } from "@react-navigation/native";
+import { useLoginMutation } from "../../redux/services/auth.service";
+import {
+  validateEmail,
+  validatePassword,
+} from "../../utils/validations/authValidation";
+import Toast from "react-native-toast-message";
 
 const LoginScreenContainer = () => {
   const navigation = useNavigation();
+  const [login, { isLoading }] = useLoginMutation();
 
-  const handleLoginButton = () => {
-    navigation.replace("Main");
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (name, value) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleLogin = async () => {
+    if (!validateEmail(formData.email) || !validatePassword(formData.password))
+      return;
+
+    try {
+      const result = await login(formData).unwrap();
+      console.log(result);
+      if (result?.isSuccess === false && result.message) {
+        Toast.show({
+          type: "error",
+          text1: result.message,
+          text1Style: {
+            fontSize: 15,
+            fontWeight: "500",
+          },
+          position: "bottom",
+        });
+      }
+
+      navigation.replace("Main");
+    } catch (error) {
+      console.log("catch: ", error);
+      alert(error?.data?.message);
+    } finally {
+      setFormData({
+        email: "",
+        password: "",
+      });
+    }
   };
 
   return (
     <LoginScreenUI
+      formData={formData}
       navigation={navigation}
-      handleLoginButton={handleLoginButton}
+      isLoading={isLoading}
+      handleChange={handleChange}
+      handleLogin={handleLogin}
     />
   );
 };
--- screens/LoginScreen/LoginScreen.ui.jsx
+++ screens/LoginScreen/LoginScreen.ui.jsx
@@ -3,55 +3,82 @@ import {
   View,
   Text,
   TextInput,
+  ActivityIndicator,
   TouchableOpacity,
   ImageBackground,
 } from "react-native";
 import styles from "./LoginScreen.styles";
 
-const LoginScreenUI = ({ navigation, handleLoginButton }) => {
+const LoginScreenUI = ({
+  navigation,
+  handleLogin,
+  isLoading,
+  handleChange,
+  formData,
+}) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.mainContainer}>
-        <ImageBackground
-          source={require("../../assets/quote-logo.png")}
-          style={styles.logo}
-          resizeMode="contain"
-        />
-      </View>
-      <View style={styles.bottomContainer}>
-        <View style={styles.bottomMainContainer}>
-          <View style={styles.titleContainer}>
-            <Text style={styles.title}>Welcome Back</Text>
-          </View>
-          <View style={styles.authContainer}>
-            <View style={styles.inputContainer}>
-              <TextInput style={styles.input} placeholder="Email" />
-              <TextInput style={styles.input} placeholder="Password" />
+    <>
+      <View style={styles.container}>
+        <View style={styles.mainContainer}>
+          <ImageBackground
+            source={require("../../assets/quote-logo.png")}
+            style={styles.logo}
+            resizeMode="contain"
+          />
+        </View>
+        <View style={styles.bottomContainer}>
+          <View style={styles.bottomMainContainer}>
+            <View style={styles.titleContainer}>
+              <Text style={styles.title}>Welcome Back</Text>
             </View>
-            <View style={styles.controlContainer}>
-              <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+            <View style={styles.authContainer}>
+              <View style={styles.inputContainer}>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Email"
+                  inputMode="email"
+                  keyboardType="email-address"
+                  onChangeText={(value) => handleChange("email", value)}
+                  value={formData.email}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Password"
+                  secureTextEntry
+                  value={formData.password}
+                  onChangeText={(value) => handleChange("password", value)}
+                />
+              </View>
+              <View style={styles.controlContainer}>
+                <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+              </View>
+              <View style={styles.authButtonContainer}>
+                <TouchableOpacity
+                  style={styles.authButton}
+                  onPress={handleLogin}
+                >
+                  <Text style={styles.authButtonText}>Sign in</Text>
+                </TouchableOpacity>
+              </View>
             </View>
-            <View style={styles.authButtonContainer}>
-              <TouchableOpacity
-                style={styles.authButton}
-                onPress={handleLoginButton}
+            <View style={styles.signUpNavContainer}>
+              <Text>Don't have an account?</Text>
+              <Text
+                style={styles.signUpText}
+                onPress={() => navigation.navigate("Register")}
               >
-                <Text style={styles.authButtonText}>Sign in</Text>
-              </TouchableOpacity>
+                Sign up
+              </Text>
             </View>
-          </View>
-          <View style={styles.signUpNavContainer}>
-            <Text>Don't have an account?</Text>
-            <Text
-              style={styles.signUpText}
-              onPress={() => navigation.navigate("Register")}
-            >
-              Sign up
-            </Text>
           </View>
         </View>
       </View>
-    </View>
+      {isLoading && (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="#686D76" />
+        </View>
+      )}
+    </>
   );
 };
 
