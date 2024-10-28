7d7dc9314502b7841ba6059b96f42567bbbe8d27
Berkan Serbes <berkan_serbes@hotmail.com>
2024-10-28 15:42:04 +0300

    refactor: Add userApi to redux store middleware

--- redux/store.js
+++ redux/store.js
@@ -2,15 +2,21 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { categoryApi } from "./services/category.service";
 import { authApi } from "./services/auth.service";
+import { userApi } from "./services/user.service";
 
 export const store = configureStore({
   reducer: {
     [categoryApi.reducerPath]: categoryApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
+    [userApi.reducerPath]: userApi.reducer,
   },
 
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware, categoryApi.middleware),
+    getDefaultMiddleware().concat(
+      authApi.middleware,
+      categoryApi.middleware,
+      userApi.middleware
+    ),
 });
 
 setupListeners(store.dispatch);
