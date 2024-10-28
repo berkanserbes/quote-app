f0cd7c951e5571ba32968964f67fed18ca0093f2
Berkan Serbes <berkan_serbes@hotmail.com>
2024-10-28 15:44:05 +0300

    fix: Rename function name

--- redux/services/category.service.js
+++ redux/services/category.service.js
@@ -1,15 +1,16 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { getToken } from "../../utils/helpers/tokenHelper";
+import { getStorageItem } from "../../utils/helpers/tokenHelper";
 
 export const categoryApi = createApi({
   reducerPath: "categoryApi",
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.EXPO_PUBLIC_API_URL,
     prepareHeaders: async (headers) => {
-      const token = await getToken("token");
+      const token = await getStorageItem("token");
       if (token) {
         headers.set("Authorization", `Bearer ${token}`);
       }
+
       return headers;
     },
   }),
